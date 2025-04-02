a1e22c416466834076a7f7c911c2a481aa6a8851
tamayama <h.tamayama.sys24@morijyobi.ac.jp>
2025-04-02 23:29:15 +0900

    春休みの課題提出

--- spring/main.js
+++ spring/main.js
@@ -10,11 +10,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     menuButton.addEventListener('click', () => {
         sideMenu.classList.add('open');
-        console.log('メニューが開きました'); // デバッグ用
+        console.log('メニューが開きました'); 
     });
 
     closeButton.addEventListener('click', () => {
         sideMenu.classList.remove('open');
-        console.log('メニューが閉じました'); // デバッグ用
+        console.log('メニューが閉じました'); 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const imageElement = document.getElementById("toggleImage");
+    const images = [
+        "../index4_all/アイタイボクラimg/aitai1.JPG",
+        "../index4_all/アイタイボクラimg/aitai2.JPG"
+    ];
+
+    let currentIndex = 0; 
+
+    imageElement.addEventListener("click", function () {
+      
+        currentIndex = (currentIndex + 1) % images.length;
+        imageElement.src = images[currentIndex];
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const imageElement = document.getElementById("togggleImage");
+    const images = [
+        "../index4_all/7周年ライブimg/IMG_7948.JPG",
+        "../index4_all/7周年ライブimg/IMG_7949.JPG"
+    ];
+
+    let currentIndex = 0;
+
+    imageElement.addEventListener("click", function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        imageElement.src = images[currentIndex];
     });
 });
